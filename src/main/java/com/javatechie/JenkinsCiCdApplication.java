997fa77db8cbcbec8a8baffffef64138de721b86
diory.coulibaly@gmail.com <diory.coulibaly@gmail.com>
2024-02-26 22:33:56 +0000

    name optimized2

--- src/main/java/com/javatechie/JenkinsCiCdApplication.java
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -15,7 +15,8 @@ public class JenkinsCiCdApplication {
     public String greetings(@PathVariable String name) {
         //Basant kumar hota
         //sarvindra kumar
-       name= name.split(" ")[0];
+         name = "Basanta kumar hota";
+      // name= name.split(" ")[0];
         return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
     }
 
